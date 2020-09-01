3dee521a828e57794ddae0bd2de97c691d07571b
Mark Kuiack <mkuiack1@helios-h0.science.uva.nl>
2020-09-01 19:12:53 +0200

    cleaning mask and antenna flag script

--- pyscripts/calc_antflags.py
+++ pyscripts/calc_antflags.py
@@ -1,10 +1,3 @@
-
-'''
-Mark Kuiack, Aug 2020.
-Read the outputs of aoquality, as csv, detect statistical outliers, and write DPPP antenna flaggin parset. 
-'''
-
-
 import pandas as pd
 import numpy as np
 import sys
@@ -31,25 +24,26 @@ for stat_name in ["/opt/Data/mkuiack1/{0}-{1}/parsets/{0}-{1}-AOQ_STD.tsv".forma
     for key in tot_sum.keys()[4:8]:
 
 #         outliers = np.abs(tot_sum[key] - np.mean(tot_sum[key])) > 1.*np.std(tot_sum[key])
-        outliers = np.abs(tot_sum[key] - np.mean(tot_sum[key])) > 2.*mad(tot_sum[key])
+        outliers = np.abs(tot_sum[key] - np.mean(tot_sum[key])) > 1.2*mad(tot_sum[key])
 
         badstations = np.append(badstations, tot_sum[key][outliers].index)
         print stat_name, key, ":", len(tot_sum[key][outliers])
 
 all_badstations = np.unique(badstations)
+
+
+# Block out stations
+all_badstations = np.append(all_badstations, 
+                     np.array(np.linspace(0,48,49,dtype=int), dtype=str))
+all_badstations = np.unique(all_badstations)
+
+
 idx = np.argsort([x.zfill(4) for x in np.array(all_badstations, dtype=str)])
+flags = ','.join(np.array(all_badstations[idx], dtype=str))
 
 print all_badstations, "len:", len(all_badstations)
 print "output string:"
 print ','.join(np.array(all_badstations[idx], dtype=str))
-
-out_list = np.append(all_badstations, 
-                     np.array(np.linspace(24,48,25,dtype=int), dtype=str))
-
-idx = np.argsort([x.zfill(4) for x in np.unique(out_list)])
-flags = ','.join(np.unique(out_list)[idx])
-
-#flags = ','.join(np.array(all_badstations[idx], dtype=str))
 
 outF = open ("/opt/Data/mkuiack1/{0}-{1}/parsets/antflag.parset".format (SB, OBS), "w")
 outF.write('''## Flag bad baselines
