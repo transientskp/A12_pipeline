2d86a3763fcb4e31f92ae2b8c18882119c86abd4
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2021-06-16 18:42:42 +0200

    Update A12_blindpipeline.sh

--- helios_pipeline/A12_blindpipeline.sh
+++ helios_pipeline/A12_blindpipeline.sh
@@ -46,7 +46,7 @@ singularity exec -B /hddstore/idayan/:/opt/Data/,/zfs/helios/filer0/mkuiack1/:/o
         $HOME/lofar-pipeline.simg  $HOME/A12_pipeline/helios_pipeline/run_trim_a2m.sh \
         $SB $START $END $INPUT
 
-#mkdir "/zfs/helios/filer0/mkuiack1/"$OBS"/"$SLICE"_all/"
+# mkdir "/zfs/helios/filer0/mkuiack1/"$OBS"/"$SLICE"_all/"
 
 
 # run_script runs all: AARTFAAC2MS, DPPP, and WSClean
