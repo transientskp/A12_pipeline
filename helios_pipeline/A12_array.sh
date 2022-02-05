1e488df45f36fa57ab6645f97c3be7c6cccda362
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-02-05 13:12:28 +0100

    Update A12_array.sh

--- helios_pipeline/A12_array.sh
+++ helios_pipeline/A12_array.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem 20G
 #SBATCH --time 12:00:00
 #SBATCH --array=1-128%32
-
+#SBATCH -w helios-cn018,helios-cn39
 OBSSLICEFILE=$1
 
 $HOME/A12_pipeline/helios_pipeline/A12_pipelinearray.sh `sed $SLURM_ARRAY_TASK_ID'q;d' $OBSSLICEFILE` 
