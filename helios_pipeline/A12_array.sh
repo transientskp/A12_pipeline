7a727bbfddcad33bd5155b95c8d9335a2678e7dd
Ibrahim Dayan <idayan@helios-h0.science.uva.nl>
2022-03-24 00:22:31 +0100

    first modify

--- helios_pipeline/A12_array.sh
+++ helios_pipeline/A12_array.sh
@@ -3,10 +3,12 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 12
-#SBATCH --mem 20G
-#SBATCH --time 12:00:00
+#SBATCH --mem 25G
+#SBATCH --time 240:00:00
 #SBATCH --array=1-128%32
-#SBATCH -w helios-cn018,helios-cn39
+#### #####SBATCH -w helios-cn018,helios-cn39
+#SBATCH --exclude=helios-cn[013-020]
+##### #### ### #SBATCH --w helios-cn005,helios-cn006,helios-cn007,helios-cn018,helios-cn019,helios-cn020,helios-cn021,helios-cn022,helios-cn023,helios-cn024,helios-cn025,helios-cn026,helios-cn027,helios-cn028,helios-cn029,helios-cn030
 OBSSLICEFILE=$1
 
 $HOME/A12_pipeline/helios_pipeline/A12_pipelinearray.sh `sed $SLURM_ARRAY_TASK_ID'q;d' $OBSSLICEFILE` 
