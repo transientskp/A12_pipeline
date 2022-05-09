a794a532c9eb065464c0c8be841698a35161e350
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-05-09 19:08:44 +0200

    Update A12_array.sh

--- helios_pipeline/A12_array.sh
+++ helios_pipeline/A12_array.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task 8
 #SBATCH --mem 25G
 #SBATCH --time 240:00:00
-#SBATCH --array=1-128%32
+#SBATCH --array=1-2528%32
 ### ## #SBATCH --array=1-128%32
 #### ## SBATCH --exclude=helios-cn[013-020]
 ##### #### ### #SBATCH --w helios-cn005,helios-cn006,helios-cn007,helios-cn018,helios-cn019
