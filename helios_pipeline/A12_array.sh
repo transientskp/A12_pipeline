3259afc2bd6a5e84a59b6a590c056d42a303c6f6
ibrahimdyn <60265502+ibrahimdyn@users.noreply.github.com>
2022-05-09 20:09:21 +0200

    Update A12_array.sh

--- helios_pipeline/A12_array.sh
+++ helios_pipeline/A12_array.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task 8
 #SBATCH --mem 25G
 #SBATCH --time 240:00:00
-#SBATCH --array=1-128%32
+#SBATCH --array=1-50%32
 ### ## #SBATCH --array=1-128%32
 #### ## SBATCH --exclude=helios-cn[013-020]
 ##### #### ### #SBATCH --w helios-cn005,helios-cn006,helios-cn007,helios-cn018,helios-cn019
@@ -28,7 +28,7 @@ OBSSLICEFILE=$1
 
 
 START=$SLURM_ARRAY_TASK_ID
-NUMLINES=50
+NUMLINES=51
 STOP=$((SLURM_ARRAY_TASK_ID*NUMLINES))
 #STOP=$((SLURM_ARRAY_TASK_ID*NUMLINES +16000))
 START="$(($STOP - $(($NUMLINES - 1)) ))"
